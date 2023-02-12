872bf45ff5f3b8edbb671b0196cd34e0842fae9c
DREAMS <124191478+D-R-E-A-M-S@users.noreply.github.com>
2023-02-12 21:28:47 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = function ( { types: t } ) {
+    return {
+        visitor: {
+            CallExpression ( path ) {
+                if ( path.node.callee.type === 'Identifier' ) {
+                    const calleeName = path.node.callee.name;
+                    const binding = path.scope.getBinding( calleeName );
+                    if ( binding && binding.path.node.type === 'ClassDeclaration' ) {
+                        path.replaceWith( t.newExpression( t.identifier( calleeName ), path.node.arguments ) );
+                    }
+                }
+            }
+        }
+    };
+};
